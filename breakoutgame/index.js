d7e98385325595829905122d0916fd5fa4552ca6
gautam yadav <gy77044@gmail.com>
2022-03-05 14:37:30 +0530

    ff
    f

--- breakoutgame/index.js
+++ breakoutgame/index.js
@@ -113,7 +113,7 @@ function moveBall(){
   drawBall()
   checkForCollisions()
 }
-timerId = setInterval(moveBall,20)
+timerId = setInterval(moveBall,15)
 
 //check for collisions
 function checkForCollisions(){
